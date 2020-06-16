4e39b3ea757011a0c9f6701b9404801c92abac8d
dikykompoz <dikykompoz@gmail.com>
2020-06-16 12:12:00 +0700

    diky

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBanner />
+    <InstaShayna />
+    <LogoShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenBanner from '@/components/WomenBanner.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import LogoShayna from '@/components/LogoShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBanner,
+    InstaShayna,
+    LogoShayna,
+    FooterShayna
+  }
+};
+</script>
